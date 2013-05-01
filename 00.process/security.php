2a3e36722918f855a47717d5d8d124b6afb16f5d
Chris Lynch <github@planetofthepenguins.com>
2013-05-01 14:58:33 +0100

    Fixed authentication bug

--- 00.process/security.php
+++ 00.process/security.php
@@ -1,31 +1,28 @@
 <?php
 // Log the user out
 if (isset($_GET['logout'])){
-    
+    setcookie ("user", null, time() - 3600,'/');
+    header('Location: authenticate',403);
 }
 
-// Apply security checks
-if (@$_GET['q'] !== 'admin/authenticate'){
-    if (strpos(@$_GET['q'],'admin') === 0){
-        // We are in the admin directory.
-        // Time to apply some security!
-        if (isset($_COOKIE['user'])){
-            // All is well, the admin cookie is set.
-        } else {
-            header('Location: /admin/authenticate',403);
-        }
-    }    
+if (isset($_POST['user']) && isset($_POST['password'])){
+    // Validate that the username and password match
+    if ($_POST['user'] == 'admin' && MD5($_POST['password']) == 'f8e63f54bf71fdfebe98bf34e9fef3e2'){
+        // Authentication test passed
+        setcookie('user',$_POST['user'],0,'/');
+        header('Location: home',301);
+    } else {
+        header('Location: authenticate',403);
+    }
 } else {
-    if (isset($_POST['user']) && isset($_POST['password'])){
-        // Validate that the username and password match
-        if ($_POST['user'] == 'admin' && MD5($_POST['password']) == 'd1c33fc99d2894f3bb5dbb1aa0e16051'){
-            // Authentication test passed
-            setcookie('user',$_POST['user'],0,'/');
-            header('Location: /admin',301);
+    if(!($this->p == 'authenticate')){
+        if (isset($_COOKIE['user'])){
+        // All is well, the admin cookie is set.
         } else {
-            header('Location: /admin/authenticate',403);
+            header('Location: authenticate',403);
         }
     }
 }
+
 
 ?>
